d2045618127b63e08f6185b76723be0845ecc86f
Juuso Valli <jvalli@cimpress.com>
2018-10-23 08:48:39 +0200

    Add preference to swap players who have high overlaps

--- match_up.py
+++ match_up.py
@@ -1,5 +1,9 @@
 from random import shuffle, choice, random
 from copy import deepcopy
+from time import time
+
+def millis():
+    return int(round(time() * 1000))
 
 def read_games(filename):
     games = []
@@ -25,7 +29,23 @@ def create_pairings(game):
     if previous != None:
         pairings.append(pair(previous, players[0]))
     return pairings
-            
+
+def validate_pairings(games, pairings):
+    for game, game_pairings in zip(games, pairings):
+        player_occurrances = {}
+        for pair in game_pairings:
+            player_occurrances[pair[0]] = player_occurrances.get(pair[0], 0) + 1
+            player_occurrances[pair[1]] = player_occurrances.get(pair[1], 0) + 1
+        assert len(game) == len(player_occurrances),'Every player should occur at least once in a pairing'
+        should_see_player_with_two_matches = len(game) % 2 == 1
+        for player, count in player_occurrances.items():
+            if count == 2 and should_see_player_with_two_matches:
+                count = 1
+                should_see_player_with_two_matches = False
+            assert count == 1,'Player %s has too many matches (%i)'%(player, count)
+                
+        
+
 def pair(a, b):
     if a == b:
         raise ValueError('Player %s cannot play with themselves' % a)
@@ -49,6 +69,8 @@ def calculate_score(pairings_list):
     return duplicates
 
 def swap_random_players(pairings):
+    ''' Create a new pairing list by swapping players in two random pairs within a random game
+    '''
     new_pairings = deepcopy(pairings)
     illegal_swap = True
     while illegal_swap:
@@ -66,25 +88,85 @@ def swap_random_players(pairings):
         match.append(pair(first_pair[0], second_pair[1]))
         match.append(pair(first_pair[1], second_pair[0]))
     return new_pairings
-    
-def exponential_temperature(t):
-    return 0.999 ** t
 
-def simulated_annealing(games, T=exponential_temperature, stopping_temperature = 0.0000000001):
+def swap_pairs_with_high_overlaps(pairings):
+    pairings = deepcopy(pairings)
+    overlaps = {}
+    for i in range(len(pairings)):
+        for i_pair in pairings[i]:
+            if i_pair not in overlaps:
+                overlaps[i_pair] = []
+            overlaps[i_pair].append(i)
+    max_overlaps = max(map(len, overlaps.values()))
+    max_pairs = {pair: games for pair, games in overlaps.items() if len(games) == max_overlaps}
+    
+    illegal_swap = True
+    attempt_count = 0
+    while illegal_swap and attempt_count < 10:
+        first_pair = choice(list(max_pairs.keys()))
+        game_index = choice(overlaps[first_pair])
+        match = pairings[game_index]
+        second_pair = choice(match)
+        if len({first_pair[0], first_pair[1], second_pair[0], second_pair[1]}) == 4:
+            illegal_swap = False
+        attempt_count += 1
+    if illegal_swap:
+        # Couldn't find a good pair to swap, delegating to random
+        print('Delegate to random')
+        return swap_random_players(pairings)
+    match.remove(first_pair)
+    match.remove(second_pair)
+    if random() < 0.5:
+        match.append(pair(first_pair[0], second_pair[0]))
+        match.append(pair(first_pair[1], second_pair[1]))
+    else:
+        match.append(pair(first_pair[0], second_pair[1]))
+        match.append(pair(first_pair[1], second_pair[0]))
+    return pairings
+        
+    
+    
+def exponential_temperature(alpha):
+    return lambda t: 0.999 ** t
+
+def simulated_annealing(games, pair_swap=swap_pairs_with_high_overlaps,T=exponential_temperature(0.999), stopping_temperature = 0.0000000001):
     pairings = list(map(create_pairings, games))
     t = 0
     while T(t) > stopping_temperature and calculate_score(pairings) > 0:
+        #print('Temperature %f' % T(t))
         current_score = calculate_score(pairings)
-        new_pairings = swap_random_players(pairings)
+        new_pairings = pair_swap(pairings)
         new_score = calculate_score(new_pairings)
         if current_score > new_score or random() < T(t):
             pairings = new_pairings
         t += 1
     return pairings
 
-games = read_games('games-50.txt')
-pairings = list(map(create_pairings, games))
-print('Random pairings give %i duplicate pairs' % calculate_score(pairings))
+def write_pairings(filename, pairings):
+    with open(filename, 'w') as f:
+        for i, game_pairings in enumerate(pairings):
+            f.write('Game %i\n' % (i + 1))
+            for pairing in game_pairings:
+                f.write('%s vs %s\n' % pairing)
+            f.write('\n')
 
-simulated_annealing_pairs = simulated_annealing(games)
-print('Simulated annealing pairings give %i duplicate pairs' % calculate_score(simulated_annealing_pairs))
+games = read_games('games-50.txt')
+s = millis()
+pairings = list(map(create_pairings, games))
+e = millis()
+validate_pairings(games, pairings)
+print('Random pairings gave %i duplicate pairs in %i ms' % (calculate_score(pairings), e - s))
+
+s = millis()
+pairings = simulated_annealing(games, pair_swap=swap_random_players)
+e = millis()
+validate_pairings(games, pairings)
+print('Simulated annealing with random swaps gave %i duplicate pairs in %i ms' % (calculate_score(pairings), e - s))
+
+s = millis()
+pairings = simulated_annealing(games)
+e = millis()
+validate_pairings(games, pairings)
+print('Simulated annealing with high overlap preference gave %i duplicate pairs in %i ms' % (calculate_score(pairings), e - s))
+
+write_pairings('result.txt', pairings)
